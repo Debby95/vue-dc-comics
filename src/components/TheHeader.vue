8ad994e47d77c331bd1393dd4007c246c6fd13c1
Debby95 <debbyjacksonstar@hotmail.it>
2022-06-21 15:37:56 +0200

    crea navbar

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="container d-flex justify-content-between">
+        <img src="/img/dc-logo.png" alt="">
+
+        <ul class="nav">
+            <li class="nav-item" v-for="(link, i) in navLinks" :key="i"> 
+                <a class="nav-link text-dark fw-bold mt-4" href="">{{link.name}}</a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "TheHeader",
+    data() {
+        return {
+            navLinks: [
+                {
+                    name: "CHARACTERS",
+                    href: "#",
+                },
+                {
+                    name: "COMICS",
+                    href: "#",
+                },
+                {
+                    name: "MOVIES",
+                    href: "#",
+                },
+                {
+                    name: "TV",
+                    href: "#",
+                },
+                {
+                    name: "GAMES",
+                    href: "#",
+                },
+                {
+                    name: "COLLECTIBLES",
+                    href: "#",
+                },
+                {
+                    name: "VIDEOS",
+                    href: "#",
+                },
+                {
+                    name: "FANS",
+                    href: "#",
+                },
+                {
+                    name: "NEWS",
+                    href: "#",
+                },
+                {
+                    name: "SHOP",
+                    href: "#",
+                },
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+    
+</style>
